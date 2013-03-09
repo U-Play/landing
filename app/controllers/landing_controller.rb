54cd15c3b3448f9fa6c50bfab3f9ba252a0042e2
Gabriel Poça <gabrielpoca@gmail.com>
2013-03-09 17:07:26 +0000

    pre finished

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,10 +1,11 @@
 class LandingController < ::ApplicationController
-  def index 
+  def index
     @mixpanel.track 'Front page'
   end
 
   def register
     @mixpanel.track 'New registration'
+    puts "HERERERERERER REGISTER #{params.inspect}"
     redirect_to action: 'index'
   end
 end
