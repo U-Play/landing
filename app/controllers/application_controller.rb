7ae3c9c9045cea3fecd2b52620749fa2504ad8b8
Gabriel Poça <gabrielpoca@gmail.com>
2013-03-19 17:18:42 +0000

    removed unecessary files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :initialize_mixpanel
-  before_filter :initialize_gibbon
-
-  def initialize_gibbon
-    @gb = Gibbon.new GIBBON_API_KEY
-  end
-
-  def initialize_mixpanel
-    if defined?(MIXPANEL_TOKEN)
-      @mixpanel = Mixpanel::Tracker.new MIXPANEL_TOKEN, request.env
-    else
-      @mixpanel = DummyMixpanel.new
-    end
-  end
-
 end
