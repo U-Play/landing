053427f6fda76c3bb6938dff49cad2e6a4904c26
Gabriel Poça <gabrielpoca@gmail.com>
2013-03-27 13:55:37 +0000

    updated email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,16 @@
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 # ActionMailer::Base.raise_delivery_errors = true
-# ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+	address: 'smtp.gmail.com',
+	port: 587,
+	domain: 'uplaypro.com',
+	authentication: 'login',
+	user_name: 'catch@uplaypro.com',
+	password: ENV['SMTP_PASSWORD'],
+	enable_starttls_auto: true
+}
 # ActionMailer::Base.smtp_settings = {
 #   :address              => "prt22.wl-dns.com",
 #   :port                 => 465,
