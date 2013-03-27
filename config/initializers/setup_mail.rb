2fc987b53a272e2f8cb7043cad4174c62386b194
Gabriel Poça <gabrielpoca@gmail.com>
2013-03-27 17:42:30 +0000

    email working in local production

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,32 +1,10 @@
-# ActionMailer::Base.delivery_method = :sendmail
-# ActionMailer::Base.perform_deliveries = true
-# ActionMailer::Base.raise_delivery_errors = true
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-# 	address: 'smtp.gmail.com',
-# 	port: 587,
-# 	domain: 'uplaypro.com',
-# 	authentication: 'login',
-# 	user_name: 'catch@uplaypro.com',
-# 	password: ENV['SMTP_PASSWORD'],
-# 	enable_starttls_auto: true
-# }
-# ActionMailer::Base.smtp_settings = {
-#   :address              => "prt22.wl-dns.com",
-#   :port                 => 465,
-#   :domain               => "uplaypro.com",
-#   :user_name            => "hello@uplaypro.com",
-#   :password             => ENV['SMTP_PASSWORD'],
-#   :authentication       => :login,
-#   :tls 					=> true
-#   # :enable_starttls_auto => true
-# }
-ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 
 password = ''
 if Rails.env.production?
 	accounts = YAML::load(File.open('/home/deploy/config/accounts.yml')).to_hash
+	# accounts = YAML::load(File.open('/Users/gabriel/ac.yml')).to_hash
 	password = accounts['gmail']['password']
 end
 
@@ -39,6 +17,5 @@ ActionMailer::Base.smtp_settings = {
 	password: password,
 	enable_starttls_auto: true
 }
+
 ActionMailer::Base.default_url_options[:host] = "uplaypro.com"
-# Mail.register_interceptor(DevelopmentMailInterceptor)
-# Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
