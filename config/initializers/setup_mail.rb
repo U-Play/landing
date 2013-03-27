15c72cff6937d2e483e395d4fabe1d0374bac326
Gabriel Poça <gabrielpoca@gmail.com>
2013-03-27 12:22:08 +0000

    updated to sendmail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,18 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address              => "prt22.wl-dns.com",
-  :port                 => 465,
-  :domain               => "uplaypro.com",
-  :user_name            => "hello@uplaypro.com",
-  :password             => ENV['SMTP_PASSWORD'],
-  :authentication       => :login,
-  :tls 					=> true
-  # :enable_starttls_auto => true
-}
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "prt22.wl-dns.com",
+#   :port                 => 465,
+#   :domain               => "uplaypro.com",
+#   :user_name            => "hello@uplaypro.com",
+#   :password             => ENV['SMTP_PASSWORD'],
+#   :authentication       => :login,
+#   :tls 					=> true
+#   # :enable_starttls_auto => true
+# }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+# ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 # Mail.register_interceptor(DevelopmentMailInterceptor)
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
